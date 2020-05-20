f82e0de306f75f36d123ecb9669c72cde329d7cb
cbrianbet <cbrianbet@outlook.com>
2020-05-20 18:56:01 +0300

    make it better

--- authApp/views.py
+++ authApp/views.py
@@ -1,7 +1,15 @@
-from abc import ABC
-
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def restricted(request, *args, **kwargs):
+    return Response
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
